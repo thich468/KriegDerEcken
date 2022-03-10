89abef253922fa0fd3789e3dcd2f7fe741258373
thimmeschc <thimmesc@students.uni-marburg.de>
2022-03-10 23:47:22 +0100

    implemented bullet self destruction after 5 seconds

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject player;
+    private int ttl = 250;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player")){ return; }
         Destroy(gameObject); 
+    }
+
+    void FixedUpdate()
+    {
+        if(ttl == 0)
+        {
+            Destroy(gameObject);
+        }
+        ttl--;
     }
 }
